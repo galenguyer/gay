eeacf150f41ac8360f3fd8c5bf4e837ead9fc6e0
Galen Guyer <galen@galenguyer.com>
2021-04-11 16:30:50 -0400

    Print a very basic flag

--- gay.c
+++ gay.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-	puts("Hello, World!");
+	puts("");
+	puts("  \033[31;41m                 \033[0m");
+	puts("  \033[91;101m                 \033[0m");
+	puts("  \033[93;103m                 \033[0m");
+	puts("  \033[32;42m                 \033[0m");
+	puts("  \033[94;104m                 \033[0m");
+	puts("  \033[35;45m                 \033[0m");
+	puts("");
+
 	return 0;
 }
