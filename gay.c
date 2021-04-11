ca429c295f64dec48c9119085cad041050331a6a
Galen Guyer <galen@galenguyer.com>
2021-04-11 19:28:04 -0400

    Add experimental 8bit color support

--- gay.c
+++ gay.c
@@ -4,11 +4,18 @@ void set_ansi(int fg) {
 	printf("\033[%d;%dm", fg, fg - 10);
 }
 
+void set_8bit(int color) {
+	// foreground
+	//printf("\033[38;5;%dm", color);
+	// background
+	printf("\033[48;5;%dm", color);
+}
+
 void set_24bit(int r, int g, int b) {
 	// foreground
 	printf("\033[38;2;%d;%d;%dm", r, g, b);
 	// background
-	printf("\033[48;2;%d;%d;%dm", r, g, b);
+	//printf("\033[48;2;%d;%d;%dm", r, g, b);
 }
 
 void reset_colors() {
@@ -20,43 +27,49 @@ int main() {
 	// red
 	printf("  ");
 	set_ansi(41);
+	set_8bit(160);
 	set_24bit(228, 3, 3);
-	printf("                 ");
+	printf("+++++++++++++++++");
 	reset_colors();
 	printf("\n");
 	// orange
 	printf("  ");
 	set_ansi(101);
+	set_8bit(208);
 	set_24bit(255, 140, 0);
-	printf("                 ");
+	printf("+++++++++++++++++");
 	reset_colors();
 	printf("\n");
 	// yellow
 	printf("  ");
 	set_ansi(103);
+	set_8bit(190);
 	set_24bit(255, 237, 0);
-	printf("                 ");
+	printf("+++++++++++++++++");
 	reset_colors();
 	printf("\n");
 	// green
 	printf("  ");
 	set_ansi(42);
+	set_8bit(28);
 	set_24bit(0, 128, 38);
-	printf("                 ");
+	printf("+++++++++++++++++");
 	reset_colors();
 	printf("\n");
 	// blue
 	printf("  ");
 	set_ansi(104);
+	set_8bit(63);
 	set_24bit(0, 77, 255);
-	printf("                 ");
+	printf("+++++++++++++++++");
 	reset_colors();
 	printf("\n");
-	// violet
+	// purple
 	printf("  ");
 	set_ansi(45);
+	set_8bit(19);
 	set_24bit(117, 7, 135);
-	printf("                 ");
+	printf("+++++++++++++++++");
 	reset_colors();
 	printf("\n");
 
