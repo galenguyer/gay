dd25a3462a874d7170530a2d5d4c5296185e49a6
Galen Guyer <galen@galenguyer.com>
2021-04-11 19:03:48 -0400

    Break setting colors into individual methods

--- gay.c
+++ gay.c
@@ -1,14 +1,67 @@
 #include <stdio.h>
 
+void set_ansi(int fg) {
+	printf("\033[%d;%dm", fg, fg - 10);
+}
+
+void set_24bit(int r, int g, int b) {
+	// foreground
+	printf("\033[38;2;%d;%d;%dm", r, g, b);
+	// background
+	printf("\033[48;2;%d;%d;%dm", r, g, b);
+}
+
+void reset_colors() {
+	printf("\033[0m");
+}
+
 int main() {
 	puts("");
-	puts("  ""\033[31;41m" "\033[38;2;228;3;3m"  "\033[48;2;228;3;3m"  "                 ""\033[0m");
-	puts("  ""\033[91;101m""\033[38;2;255;140;0m""\033[48;2;255;140;0m""                 ""\033[0m");
-	puts("  ""\033[93;103m""\033[38;2;255;237;0m""\033[48;2;255;237;0m""                 ""\033[0m");
-	puts("  ""\033[32;42m" "\033[38;2;0;128;38m" "\033[48;2;0;128;38m" "                 ""\033[0m");
-	puts("  ""\033[94;104m""\033[38;2;0;77;255m" "\033[48;2;0;77;255m" "                 ""\033[0m");
-	puts("  ""\033[35;45m" "\033[38;2;117;7;135m""\033[48;2;117;7;135m""                 ""\033[0m");
+	// red
+	printf("  ");
+	set_ansi(41);
+	set_24bit(228, 3, 3);
+	printf("                 ");
+	reset_colors();
+	printf("\n");
+	// orange
+	printf("  ");
+	set_ansi(101);
+	set_24bit(255, 140, 0);
+	printf("                 ");
+	reset_colors();
+	printf("\n");
+	// yellow
+	printf("  ");
+	set_ansi(103);
+	set_24bit(255, 237, 0);
+	printf("                 ");
+	reset_colors();
+	printf("\n");
+	// green
+	printf("  ");
+	set_ansi(42);
+	set_24bit(0, 128, 38);
+	printf("                 ");
+	reset_colors();
+	printf("\n");
+	// blue
+	printf("  ");
+	set_ansi(104);
+	set_24bit(0, 77, 255);
+	printf("                 ");
+	reset_colors();
+	printf("\n");
+	// violet
+	printf("  ");
+	set_ansi(45);
+	set_24bit(117, 7, 135);
+	printf("                 ");
+	reset_colors();
+	printf("\n");
+
 	puts("");
 
 	return 0;
 }
+
