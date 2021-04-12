c38397cb5453c880c40a529c7e3f978b4d94fd0a
Galen Guyer <galen@galenguyer.com>
2021-04-11 22:48:06 -0400

    Create print_line method for each color

--- gay.c
+++ gay.c
@@ -30,63 +30,39 @@ void reset_colors() {
 	printf("\033[0m");
 }
 
+void print_line(struct color c, int length) {
+	printf("  ");
+	set_ansi(c.ansi);
+	set_8bit(c.xterm);
+	set_24bit(c.r, c.g, c.b);
+	for (int i = 0; i < length; i++) {
+		printf(" ");
+	}
+	reset_colors();
+	printf("\n");
+}
+
 int main() {
 	puts("");
 
 	// red
 	struct color red = { .ansi = 41, .xterm = 160, .r = 228, .g = 3, .b = 3 };
-	printf("  ");
-	set_ansi(red.ansi);
-	set_8bit(red.xterm);
-	set_24bit(red.r, red.g, red.b);
-	printf("                 ");
-	reset_colors();
-	printf("\n");
+	print_line(red, 17);
 	// orange
 	struct color orange = { .ansi = 101, .xterm = 208, .r = 255, .g = 140, .b = 0 };
-	printf("  ");
-	set_ansi(orange.ansi);
-	set_8bit(orange.xterm);
-	set_24bit(orange.r, orange.g, orange.b);
-	printf("                 ");
-	reset_colors();
-	printf("\n");
+	print_line(orange, 17);
 	// yellow
 	struct color yellow = { .ansi = 103, .xterm = 226, .r = 255, .g = 237, .b = 0 };
-	printf("  ");
-	set_ansi(yellow.ansi);
-	set_8bit(yellow.xterm);
-	set_24bit(yellow.r, yellow.g, yellow.b);
-	printf("                 ");
-	reset_colors();
-	printf("\n");
+	print_line(yellow, 17);
 	// green
 	struct color green = { .ansi = 42, .xterm = 28, .r = 0, .g = 128, .b = 38 };
-	printf("  ");
-	set_ansi(green.ansi);
-	set_8bit(green.xterm);
-	set_24bit(green.r, green.g, green.b);
-	printf("                 ");
-	reset_colors();
-	printf("\n");
+	print_line(green, 17);
 	// blue
 	struct color blue = { .ansi = 104, .xterm = 27, .r = 0, .g = 77, .b = 255 };
-	printf("  ");
-	set_ansi(blue.ansi);
-	set_8bit(blue.xterm);
-	set_24bit(blue.r, blue.g, blue.b);
-	printf("                 ");
-	reset_colors();
-	printf("\n");
+	print_line(blue, 17);
 	// purple
 	struct color purple = { .ansi = 45, .xterm = 90, .r = 117, .g = 7, .b = 135 };
-	printf("  ");
-	set_ansi(purple.ansi);
-	set_8bit(purple.xterm);
-	set_24bit(purple.r, purple.g, purple.b);
-	printf("                 ");
-	reset_colors();
-	printf("\n");
+	print_line(purple, 17);
 
 	puts("");
 
