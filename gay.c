ddca732a2e90f3e870836cb97edd3bd37a5f4063
Galen Guyer <galen@galenguyer.com>
2021-04-11 22:42:43 -0400

    Switch remaining colors to color struct

--- gay.c
+++ gay.c
@@ -32,6 +32,7 @@ void reset_colors() {
 
 int main() {
 	puts("");
+
 	// red
 	struct color red = { .ansi = 41, .xterm = 160, .r = 288, .g = 3, .b = 3 };
 	printf("  ");
@@ -42,42 +43,47 @@ int main() {
 	reset_colors();
 	printf("\n");
 	// orange
+	struct color orange = { .ansi = 101, .xterm = 208, .r = 255, .g = 140, .b = 0 };
 	printf("  ");
-	set_ansi(101);
-	set_8bit(208);
-	set_24bit(255, 140, 0);
+	set_ansi(orange.ansi);
+	set_8bit(orange.xterm);
+	set_24bit(orange.r, orange.g, orange.b);
 	printf("                 ");
 	reset_colors();
 	printf("\n");
 	// yellow
+	struct color yellow = { .ansi = 103, .xterm = 226, .r = 255, .g = 237, .b = 0 };
 	printf("  ");
-	set_ansi(103);
-	set_8bit(226);
-	set_24bit(255, 237, 0);
+	set_ansi(yellow.ansi);
+	set_8bit(yellow.xterm);
+	set_24bit(yellow.r, yellow.g, yellow.b);
 	printf("                 ");
 	reset_colors();
 	printf("\n");
 	// green
+	struct color green = { .ansi = 42, .xterm = 28, .r = 0, .g = 128, .b = 38 };
 	printf("  ");
-	set_ansi(42);
-	set_8bit(28);
-	set_24bit(0, 128, 38);
+	set_ansi(green.ansi);
+	set_8bit(green.xterm);
+	set_24bit(green.r, green.g, green.b);
 	printf("                 ");
 	reset_colors();
 	printf("\n");
 	// blue
+	struct color blue = { .ansi = 104, .xterm = 27, .r = 0, .g = 77, .b = 255 };
 	printf("  ");
-	set_ansi(104);
-	set_8bit(27);
-	set_24bit(0, 77, 255);
+	set_ansi(blue.ansi);
+	set_8bit(blue.xterm);
+	set_24bit(blue.r, blue.g, blue.b);
 	printf("                 ");
 	reset_colors();
 	printf("\n");
 	// purple
+	struct color purple = { .ansi = 45, .xterm = 90, .r = 117, .g = 7, .b = 135 };
 	printf("  ");
-	set_ansi(45);
-	set_8bit(90);
-	set_24bit(117, 7, 135);
+	set_ansi(purple.ansi);
+	set_8bit(purple.xterm);
+	set_24bit(purple.r, purple.g, purple.b);
 	printf("                 ");
 	reset_colors();
 	printf("\n");
@@ -86,4 +92,3 @@ int main() {
 
 	return 0;
 }
-
